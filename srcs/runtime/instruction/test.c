240f3e8e36d260c416c8f76b4b6d10682ae47482
seruiz <seruiz@student.le-101.fr>
2021-03-15 15:56:06 +0100

    start over

--- srcs/runtime/instruction/test.c
+++ srcs/runtime/instruction/test.c
@@ -6,7 +6,7 @@
 /*   By: seruiz <seruiz@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 12:10:28 by seruiz            #+#    #+#             */
-/*   Updated: 2021/03/15 15:23:01 by seruiz           ###   ########lyon.fr   */
+/*   Updated: 2021/03/15 15:55:47 by seruiz           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,120 @@
 #include "minishell_utilities.h"
 
 #include <unistd.h>
+/*
+int		ft_is_valid_var_name(t_shell_context *context, t_shell_command *command, int i, char *str)
+{
+	int		j;
+	int		k;
+	char	*key;
+	char	*result;
+
+	j = i;
+	k = 0;
+	while ((ft_isalnum(command->command_string[i]) == 1 || command->command_string[i] == '_') && command->command_mask[i] == '0')
+		i++;
+	if	 (i - j > 0)
+	{
+		key = malloc(sizeof(char) * (i - j + 1));
+		key[i - j] = '\0';
+		i = j;
+		while ((ft_isalnum(command->command_string[i]) == 1 || command->command_string[i] == '_') && command->command_mask[i] == '0')
+		{
+			key[k] = command->command_string[i];
+			k++;
+			i++;
+		}
+		printf("key = %s\n", key);
+		printf("value = %s\n", env_get(context, key));
+		printf("str = %s\n", str);
+
+		result = ft_strjoin(str, (char *)env_get(context, key));
+		free(str);
+		str = result;
+
+	}
+	return (i - j);
+}
+
+int		ft_replace_env_variable(t_shell_context *context, t_shell_command *command, int i, char *str)
+{
+	int		var_len;
+	//char	*key;
+
+	var_len = ft_is_valid_var_name(context, command, i, str);
+	return (var_len + 1);
+
+//	while (j < i)
+//	{
+//
+//	}
+//	env_get(t_shell_context *context, char *key)
+}
+
+int	ft_cpy_str(t_shell_context *context, t_shell_command *command, int i, char *str)
+{
+	int		j;
+	int		k;
+	int		backslash;
+	char	*result;
+
+	(void)context;
+	backslash = 0;
+	k = 0;
+	j = i;
+	while (command->command_string[i] && command->command_string[i] != '$' && (command->command_string[i] != '>' || command->command_string[i] != '<'))
+	{
+		if (command->command_string[i] == '\\')
+		{
+			backslash++;
+			i++;
+		}
+		i++;
+	}
+	result = malloc(sizeof(char) * (i - j - backslash + 1));
+	printf("result len = %d\n", i - j - backslash + 1);
+	result[i - j - backslash] = '\0';
+	while (j < i)
+	{
+		if (command->command_string[j] == '\\')
+			j++;
+		result[k] = command->command_string[j];
+		k++;
+		j++;
+	}
+	str = ft_strjoin(str, result);
+	free(result);
+	printf("str = %s\n", str);
+	return (i);
+}
+
+void	ft_test(t_shell_context *context, t_shell_command *command)
+{
+	int		i;
+	//char	**str_tab;
+	int		j;
+	char	*str;
+
+	(void)j;
+	i = 0;
+	str = malloc(sizeof(char));
+	str[0] = '\0';
+	printf("string = %s\n", command->command_string);
+	printf("mask   = %s\n", command->command_mask);
+
+	while (command->command_string[i])
+	{
+		if (command->command_string[i] == '$')
+			i += ft_replace_env_variable(context, command, i + 1, str);
+		else
+			i = ft_cpy_str(context, command, i, str);
+		printf("str in ft_test = %s\n", str);
+	}
+}
+
+//==============================================================================
+//==============================================================================
+//==============================================================================
 
 int		ft_is_valid_var_name(t_shell_context *context, t_shell_command *command, int i, char *str)
 {
@@ -57,16 +171,9 @@ int		ft_replace_env_variable(t_shell_context *context, t_shell_command *command,
 
 	var_len = ft_is_valid_var_name(context, command, i, str);
 	return (var_len + 1);
-	/*
-	while (j < i)
-	{
-
-	}
-	env_get(t_shell_context *context, char *key)
-	*/
 }
 
-int	ft_cpy_str(t_shell_context *context, t_shell_command *command, int i, char *str)
+int	ft_cpy_str(t_shell_context *context, t_shell_command *command, int i, char **str)
 {
 	int		j;
 	int		k;
@@ -97,14 +204,11 @@ int	ft_cpy_str(t_shell_context *context, t_shell_command *command, int i, char *
 		k++;
 		j++;
 	}
-	if (str != NULL)
-	{
-		str = ft_strjoin(str, result);
-		free(str);
-	}
-	else
-		str = result;
-	printf("str = %s\n", str);
+	if (str[0] == NULL)
+		str[0] = result;
+	str[0] = ft_strjoin(str[0], result);
+	free(result);
+	printf("str = %s\n", str[0]);
 	return (i);
 }
 
@@ -113,35 +217,32 @@ void	ft_test(t_shell_context *context, t_shell_command *command)
 	int		i;
 	//char	**str_tab;
 	int		j;
-	char	*str;
+	char	**str;
 
 	(void)j;
 	i = 0;
-	str = NULL;
+	str = malloc(sizeof(char *));
 	printf("string = %s\n", command->command_string);
 	printf("mask   = %s\n", command->command_mask);
-
-	//Calculer la taille de chaque string et ensuite la copier dans argv
-
-
 
 	while (command->command_string[i])
 	{
 		if (command->command_string[i] == '$')
-			i += ft_replace_env_variable(context, command, i + 1, str);
+			i += ft_replace_env_variable(context, command, i + 1, str[0]);
 		else
 			i = ft_cpy_str(context, command, i, str);
-		printf("str in ft_test = %s\n", str);
-		/*
-		if (command->command_string[i] == '\\')
-		{
-			
-		}
-		if (command->command_string[i] == '$')
-		{
-			ft_replace_env_variable(command, str);
-		}
-		*/
+		printf("str in ft_test = %s\n", str[0]);
 	}
+}
+*/
 
+void	ft_test(t_shell_context *context, t_shell_command *command)
+{
+	int	i;
+
+	i = 0;
+	while (command->command_string[i])
+	{
+		i++;
+	}
 }
